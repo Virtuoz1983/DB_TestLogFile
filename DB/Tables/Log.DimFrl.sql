fd21c0996b8ced81fe48f4d7a281e5dbcec9792f
Valerii Tereschenko <Valerii.Tereschenko@bd.com>
2019-06-05 13:53:08 +0300

    added script for Log.DimFrl table creation

---
+++ DB/Tables/Log.DimFrl.sql
@@ -0,0 +1,31 @@
+
+/*
+Table:  Log.DimFrl
+
+Purpose:  Dimenssion for storing Frl data. SCD Type-1
+
+Activities:
+
+Date		Modified By		    Comments
+05.06.2019  Labunets S.P.       First design			            
+
+Sample code:
+
+     DROP TABLE Log.DimFrl
+     TRUNCATE TABLE Log.DimFrl
+
+     SELECT * FROM Log.DimFrl
+*/
+
+
+
+CREATE TABLE [Log].[DimFrl]
+(
+	[FrlId]    int			 NOT NULL,   -- id
+	[FrlName]  NVARCHAR(200) NOT NULL,   -- Frl name
+
+	CONSTRAINT [PK_frls] PRIMARY KEY CLUSTERED ([FrlId])
+) 
+
+
+
